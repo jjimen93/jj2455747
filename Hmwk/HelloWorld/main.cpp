f441f2f7f51afcb2b34439a487a3edb0bafe4813
jjimen93 <joshue.jimenez@gmail.com>
2016-06-22 19:15:17 -0700

    Add files via upload

---
+++ Hmwk/HelloWorld/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: Joshue Jimenez
+ * Created on June 20, 2016, 6:51 PM
+ * Purpose: First Program
+ */
+
+//System Libraries
+#include <iostream>//Input//Output/ Library
+using namespace std;//Namespace of the System Libraries 
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input Data
+    
+    //Process the Data
+    
+    //Output the processed Data
+    cout<<"Hello World"<<endl;
+    
+    //Exit Stage Right!
+    return 0;
+}
